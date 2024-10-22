f8821ea8e795345437dd4401e0175814b577402a
Abdou-SYLLA <syllaa@3il.fr>
2024-10-22 22:21:12 +0200

    deployer sur azure et fonctionnel. Authentification corrigé et ajout de compte aussi

--- src/views/annonce.php
+++ src/views/annonce.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Annonces</title>
-    <link rel="stylesheet" href="../../public/styles/styles.css"> <!-- Fichier CSS partagé -->
+    <link rel="stylesheet" href="/public/styles/styles.css"> <!-- Fichier CSS partagé -->
 </head>
 <body>
     
@@ -21,7 +21,7 @@
     <header>
         
         <div class="logo">
-            <img src="../../public/data/images/logo2.png" alt="BagShare"> <!-- Chemin absolu ou relatif -->
+            <img src="/public/data/images/logo2.png" alt="BagShare"> <!-- Chemin absolu ou relatif -->
         </div>
         <nav>
             <a href="../../public/index.php">Accueil</a> <!-- Chemin absolu ou relatif -->           
@@ -30,9 +30,9 @@
                 echo "<a href=\"#ajouterAnnonce\">Ajouter</a>";
                 // Si l'utilisateur est connecté, afficher son nom et un bouton de déconnexion
                 echo "<a href='#'>" . htmlspecialchars($_SESSION['nom']) . "</a>";
-                echo "<a href='/bagshare/src/controllers/disconnect.php' class=\"btn-secondary\">Déconnexion</a>";
+                echo "<a href='/src/controllers/disconnect.php' class=\"btn-secondary\">Déconnexion</a>";
             } else {
-                echo "<a href='/bagshare/src/views/connexion.php' class=\"btn-secondary\">Connexion</a>";
+                echo "<a href='/src/views/connexion.php' class=\"btn-secondary\">Connexion</a>";
             }
             ?>
         </nav>
--- src/views/header.php
+++ src/views/header.php
@@ -1,21 +1,21 @@
 <header>
     <div class="logo">
-        <img src="/bagshare/public/data/images/logo2.png" alt="BagShare"> <!-- Chemin absolu -->
+        <img src="/public/data/images/logo2.png" alt="BagShare"> <!-- Chemin absolu -->
     </div>
     <nav>
-        <a href="/bagshare/public/index.php">Acceuil</a> <!-- Chemin absolu -->
-        <a href="/bagshare/src/views/annonce.php">Annonces</a> <!-- Chemin absolu -->
-        <a href="/bagshare/src/views/about.php">A propos</a> <!-- Chemin absolu -->
-        <a href="/bagshare/src/views/contact.php">Nous contacter</a> <!-- Chemin absolu -->
+        <a href="/public/index.php">Acceuil</a> <!-- Chemin absolu -->
+        <a href="/src/views/annonce.php">Annonces</a> <!-- Chemin absolu -->
+        <a href="/src/views/about.php">A propos</a> <!-- Chemin absolu -->
+        <a href="/src/views/contact.php">Nous contacter</a> <!-- Chemin absolu -->
 
         <?php
         session_start();
         if (isset($_SESSION['user'])) {
             // Si l'utilisateur est connecté, afficher son nom et un bouton de déconnexion
             echo "<a href='#'>" . htmlspecialchars($_SESSION['nom']) . "</a>";
-            echo "<a href='/bagshare/src/controllers/disconnect.php' class=\"btn-secondary\">Déconnexion</a>"; // Chemin absolu
+            echo "<a href='/src/controllers/disconnect.php' class=\"btn-secondary\">Déconnexion</a>"; // Chemin absolu
         } else {
-            echo "<a href='/bagshare/src/views/connexion.php' class=\"btn-secondary\">Connexion</a>"; // Chemin absolu
+            echo "<a href='/src/views/connexion.php' class=\"btn-secondary\">Connexion</a>"; // Chemin absolu
         }
         ?>
     </nav>
