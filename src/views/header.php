9b2c86ac11f205dd0927216684f8eceae189d82c
Abdou-SYLLA <syllaa@3il.fr>
2024-10-25 10:00:33 +0000

    changement de l'admin

--- src/views/header.php
+++ src/views/header.php
@@ -5,22 +5,30 @@
     <nav>
         <?php
         $current_page = basename($_SERVER['PHP_SELF']); // Obtient le nom du fichier PHP en cours
-        
-        // Définir les liens avec une condition pour la classe 'active'
         ?>
-        <a href="/public/index.php" class="<?= $current_page == 'index.php' ? 'active' : '' ?>">Accueil</a> <!-- Chemin absolu -->
-        <a href="/src/views/annonce.php" class="<?= $current_page == 'annonce.php' ? 'active' : '' ?>">Annonces</a> <!-- Chemin absolu -->
-        <a href="/src/views/about.php" class="<?= $current_page == 'about.php' ? 'active' : '' ?>">A propos</a> <!-- Chemin absolu -->
-        <a href="/src/views/contact.php" class="<?= $current_page == 'contact.php' ? 'active' : '' ?>">Nous contacter</a> <!-- Chemin absolu -->
+
+        <!-- Liens de navigation principaux -->
+        <a href="/public/index.php" class="<?= $current_page == 'index.php' ? 'active' : '' ?>">Accueil</a>
+        <a href="/src/views/annonce.php" class="<?= $current_page == 'annonce.php' ? 'active' : '' ?>">Annonces</a>
+        <a href="/src/views/about.php" class="<?= $current_page == 'about.php' ? 'active' : '' ?>">A propos</a>
+        <a href="/src/views/contact.php" class="<?= $current_page == 'contact.php' ? 'active' : '' ?>">Nous contacter</a>
 
         <?php
+        if (isset($_SESSION['user']) && $_SESSION['role'] === 'admin') {
+            // Si l'utilisateur est administrateur, afficher les options de gestion
+            echo '<a href="/src/views/user.php" class="' . ($current_page == 'user.php' ? 'active' : '') . '">Gestion Utilisateurs</a>';
+            echo '<a href="/src/views/account.php" class="' . ($current_page == 'account.php' ? 'active' : '') . '">Gestion Comptes</a>';
+        }
+
         if (isset($_SESSION['user'])) {
             // Si l'utilisateur est connecté, afficher son nom et un bouton de déconnexion
-            echo "<a href='#'>" . htmlspecialchars($_SESSION['nom']) . "</a>";
-            echo "<a href='/src/controllers/disconnect.php' class=\"btn-secondary\">Déconnexion</a>"; // Chemin absolu
+            echo '<a href="#">' . htmlspecialchars($_SESSION['nom']) . '</a>';
+            echo '<a href="/src/controllers/disconnect.php" class="btn-secondary">Déconnexion</a>';
         } else {
-            echo "<a href='/src/views/connexion.php' class=\"btn-secondary\">Connexion</a>"; // Chemin absolu
+            echo '<a href="/src/views/connexion.php" class="btn-secondary">Connexion</a>';
         }
+
+        
         ?>
     </nav>
 </header>
